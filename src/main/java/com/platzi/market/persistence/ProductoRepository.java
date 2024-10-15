9e64fe7c4d3b6362e4d92fff4ebca14ee932e857
Arnie Adriano <arnie.7u7@gmail.com>
2024-10-15 21:53:12 +0200

    Brrr

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entitys.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -32,6 +34,18 @@ public class ProductoRepository {
 
     public Optional<List<Producto>>getByPriceGreaterThanSpecificValue(Double precioVenta){
         return productoCrudRepository.findByPrecioVentaGreaterThanEqual(precioVenta);
+    }
+
+    public Optional<Producto>getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 
 }
