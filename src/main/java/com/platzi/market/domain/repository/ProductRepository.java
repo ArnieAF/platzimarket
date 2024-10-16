706b6d4933eeaaf5169e57b484413b2562780a13
Arnie Adriano <arnie.7u7@gmail.com>
2024-10-16 07:24:36 +0200

    commit8

--- src/main/java/com/platzi/market/domain/repository/ProductRepository.java
+++ src/main/java/com/platzi/market/domain/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.platzi.market.domain.repository;
 
 import com.platzi.market.domain.Product;
+import com.platzi.market.persistence.entitys.Producto;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,8 +12,8 @@ public interface ProductRepository {
     Optional<List<Product>> getByCategory(int categoryId);
     Optional<List<Product>> getScarseProducts(int quantity);
     Optional<List<Product>> getByBarCode(String barcode);
-    Optional<List<Product>> getByRangeOfPrices(Double min, Double max);
-    Optional<List<Product>>getByPriceGreaterThanSpecificValue(Double salePrice);
+    Optional<List<Producto>> getByRangeOfPrices(Double min, Double max);
+    Optional<List<Producto>> getByPriceGreaterThanSpecificValue(Double salePrice);
     Optional<Product> getProduct(int productId);
     Product save(Product product);
     void delete(int productId);
